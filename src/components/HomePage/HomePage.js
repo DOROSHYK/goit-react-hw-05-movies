5b312820159a1354be809df433dfb7bf1456bcfa
Doroshuk <dorosukolga100@gmail.com>
2021-09-21 16:38:39 +0300

    дещо по стилю

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 import { getTrendingMovies } from '../../services/api';
 import routes from '../../routes';
 import Spinner from '../Spinner/Spinner';
-
+import st from './HomePage.module.css';
 class HomePage extends Component {
   state = {
     films: [],
@@ -22,27 +22,35 @@ class HomePage extends Component {
   render() {
     const { films, loading } = this.state;
     return (
-      <div>
-        <h2>Trending this week</h2>
-        {loading ? (
-          <Spinner />
-        ) : films.length ? (
-          <ul>
-            {films.map(film => (
-              <li key={film.id}>
-                <NavLink
-                  to={{
-                    pathname: `${routes.movies}/${film.id}`,
-                    state: { from: this.props.location },
-                  }}
-                >
-                  {film.title ? film.title : film.original_name}
-                </NavLink>
-              </li>
-            ))}
-          </ul>
-        ) : null}
-      </div>
+      <section className={st.container}>
+        <div className={st.filmCardContainer}>
+          <h2>Trending this week</h2>
+          {loading ? (
+            <Spinner />
+          ) : films.length ? (
+            <ul className={st.filmCard}>
+              {films.map(film => (
+                <li key={film.id} className={st.filmCardList}>
+                  <NavLink
+                    to={{
+                      pathname: `${routes.movies}/${film.id}`,
+                      state: { from: this.props.location },
+                    }}
+                    className={st.filmCardLink}
+                  >
+                    <img
+                      src={`https://image.tmdb.org/t/p/w500/${film.poster_path}`}
+                      alt=""
+                      className={st.posterSize}
+                    />
+                    {film.title ? film.title : film.original_name}
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+          ) : null}
+        </div>
+      </section>
     );
   }
 }
